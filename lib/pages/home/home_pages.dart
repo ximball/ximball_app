3b65000d513ed89928ef33d9380ea274ac232bc2
IsraelMerlyn <merlyn142a@gmail.com>
2022-09-27 22:57:52 -0500

    actualizacion del levantamiento

--- lib/pages/home/home_pages.dart
+++ lib/pages/home/home_pages.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[200],
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: SafeArea(
         child: Column(
           children: [
