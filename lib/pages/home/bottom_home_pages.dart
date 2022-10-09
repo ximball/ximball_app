76331e95eb4f550059bddb5d93ef46d9c643f3fc
IsraelMerlyn <merlyn142a@gmail.com>
2022-10-09 13:34:21 -0500

    update correcciones

--- lib/pages/home/bottom_home_pages.dart
+++ lib/pages/home/bottom_home_pages.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_final_fields, unused_field
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, unused_field, unused_import
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
